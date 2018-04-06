15ef68dc7f787769174e7230c6690bd88fd907e6
Cody Raffensperger <cody.raffensperger@gmail.com>
2018-04-06 16:23:01 -0400

    -User Twitter library tweetinvi to start searching tweets.

--- TwitterApiSandbox/Program.cs
+++ TwitterApiSandbox/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 
 namespace TwitterApiSandbox
 {
@@ -10,6 +9,33 @@ namespace TwitterApiSandbox
     {
         static void Main(string[] args)
         {
+            Auth.SetUserCredentials(
+                Properties.Settings.Default.ConsumerKey,
+                Properties.Settings.Default.ConsumerSecret,
+                Properties.Settings.Default.AccessToken,
+                Properties.Settings.Default.AccessTokenSecret);
+
+
+            var searchParameter = new SearchTweetsParameters("Trump")
+            {
+                //GeoCode = new GeoCode(-122.398720, 37.781157, 1, DistanceMeasure.Miles),
+                Lang = LanguageFilter.English,
+                SearchType = SearchResultType.Recent,
+                MaximumNumberOfResults = 15,
+                //Until = new DateTime(2015, 06, 02),
+                //SinceId = 399616835892781056,
+                //MaxId = 405001488843284480,
+                //Filters = TweetSearchFilters.Images
+            };
+
+            var tweets = Search.SearchTweets(searchParameter);
+
+            foreach (var tweet in tweets)
+            {
+                Console.WriteLine($"Tweet: {tweet.FullText}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
